26175ad67c5dc88d978da64dabb3ca025b181cad
Shaun Lawrence <shaunrlawrence@gmail.com>
2022-08-12 22:04:20 +0100

    Restrict Orbit game to landscape only

--- games/Orbit/Orbit/Platforms/Android/MainActivity.cs
+++ games/Orbit/Orbit/Platforms/Android/MainActivity.cs
@@ -4,7 +4,17 @@ using Android.OS;
 
 namespace Orbit;
 
-[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+[Activity(
+    Theme = "@style/Maui.SplashTheme",
+    MainLauncher = true,
+    ConfigurationChanges =
+        ConfigChanges.ScreenSize |
+        ConfigChanges.Orientation |
+        ConfigChanges.UiMode |
+        ConfigChanges.ScreenLayout |
+        ConfigChanges.SmallestScreenSize |
+        ConfigChanges.Density,
+    ScreenOrientation = ScreenOrientation.Landscape)]
 public class MainActivity : MauiAppCompatActivity
 {
 }
