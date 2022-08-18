1d44dbdc13621555599ebb0d262d1a9a310bb0d5
Shaun Lawrence <shaunrlawrence@gmail.com>
2022-08-18 23:11:33 +0100

    Messing with more snapshot testing thoughts

--- games/Orbit/Orbit.Tests/UnitTest1.cs
+++ games/Orbit/Orbit.Tests/UnitTest1.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Maui.Graphics.Skia;
+﻿using System.Runtime.CompilerServices;
+using Microsoft.Maui.Graphics.Skia;
 using NUnit.Framework;
 using Orbit.Engine;
 using Orbit.GameObjects;
+using Orbit.Scenes;
 using SkiaSharp;
 
 namespace Orbit.Tests;
@@ -14,14 +16,31 @@ public class Tests
         VerifyScene(new Scenery(), 1000, 1000);
     }
 
-    // TODO: For some reason the asteroid is not rendering.
-    //[Test]
-    //public void AsteroidShouldRenderAsExpected()
-    //{
-    //    VerifyScene(new Asteroid(null), 1000, 1000);
-    //}
+    [Test]
+    public void HomeSceneShouldRenderAsExpected()
+    {
+        var home = new HomeScene(new Ship(null, new Gun()), new Sun(), new Planet(null, new GameObjects.Shadow()));
+        home.Update(0);
+        VerifyScene(home, 1000, 1000);
+    }
 
-    public static void VerifyScene(IRender render, int sceneWidth, int sceneHeight)
+    // TODO: For some reason the asteroid is not rendering.
+    [Test]
+    public void AsteroidShouldRenderAsExpected()
+    {
+        var asteroid = new Asteroid(null);
+
+        asteroid.SetMovement(new Movement(new PointF(0f, 0f), new PointF(0.5f, 0.5f), new PointF(0.01f, 0.01f)));
+
+        for (int i = 0; i < 10; i++)
+        {
+            VerifyScene(asteroid, 1000, 1000, $"AsteroidShouldRenderAsExpected{i}");
+
+            asteroid.Update(16);
+        }
+    }
+
+    public static void VerifyScene(IRender render, int sceneWidth, int sceneHeight, [CallerMemberName] string caller = "")
     {
         using var canvas = new SkiaCanvas();
         using var bitmap = new SKBitmap(new SKImageInfo(sceneWidth, sceneHeight));
@@ -30,7 +49,7 @@ public class Tests
         render.Render(canvas, new Microsoft.Maui.Graphics.RectF(0, 0, sceneWidth, sceneHeight));
 
         // TODO: apply test name to the file.
-        using var stream = File.Create("snapshot.png");
+        using var stream = File.Create($"{caller}.png");
         bitmap.Encode(stream, SKEncodedImageFormat.Png, 100);
     }
 
