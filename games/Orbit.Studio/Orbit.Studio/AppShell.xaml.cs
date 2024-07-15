3992e62c267e79d6b34403c0d5fa48c882d6c0a7
Shaun Lawrence <17139988+bijington@users.noreply.github.com>
2024-07-15 20:18:27 +0100

    Shell application

---
+++ games/Orbit.Studio/Orbit.Studio/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Orbit.Studio;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ games/Orbit.Studio/Orbit.Studio/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Orbit.Studio;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
