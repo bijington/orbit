d502c2f48465fa023fbb883bdd2c276c0a4755e1
Shaun Lawrence <17139988+bijington@users.noreply.github.com>
2025-03-05 18:12:39 +0000

    Introduce a PowerShell script to manage packing and publishing of multiple different packages

---
+++ scripts/package-releases.ps1
@@ -0,0 +1,38 @@
+param (
+    [string]$Directory,
+    [string]$Version,
+    [string]$ApiKey
+)
+
+$ProjectFilter = "*.csproj"
+
+if ($Version -like "v*") {
+    $Version = $Version.Substring(1)
+} elseif ($Version -like "input*") {
+    $Version = $Version.Substring(5)
+    $ProjectFilter = "Orbit.Input.csproj"
+} elseif ($Version -like "engine*") {
+    $Version = $Version.Substring(6)
+    $ProjectFilter = "Orbit.Engine.csproj"
+}
+
+Get-ChildItem -Path $Directory -Recurse -Filter $ProjectFilter |
+    Where-Object { $_.Name -notlike "*Test*" } |
+    ForEach-Object {
+        Write-Host "Restoring $($_.FullName)"
+        dotnet restore $_
+
+        Write-Host "Building $($_.FullName)"
+        dotnet build --configuration Release --no-restore $_ /p:Version=$Version
+
+        Write-Host "Packing $($_.FullName)"
+        dotnet pack $_ -c Release /p:Version=$Version --no-build --output .
+
+        $PackageWithVersion = "Bijington.$($_.Directory.Name).$Version"
+
+        Write-Host "Pushing $PackageWithVersion.nupkg"
+        dotnet nuget push "$PackageWithVersion.nupkg" -s https://api.nuget.org/v3/index.json -k $ApiKey
+
+        Write-Host "Pushing $PackageWithVersion.snupkg"
+        dotnet nuget push "$PackageWithVersion.snupkg" -s https://api.nuget.org/v3/index.json -k $ApiKey
+    }
